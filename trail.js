c177822853a21caef522fd8320e7a40bfc6452bd
diversity-tottori <54832971+diversity-tottori@users.noreply.github.com>
2021-06-11 22:14:51 +0900

    Update trail.js

--- trail.js
+++ trail.js
@@ -1,38 +1,31 @@
-var map = L.map('map'),
-    trail = {
-        type: 'Feature',
-        properties: {
-            id: 1
+function createRealtimeLayer(url, container) {
+    return L.realtime(url, {
+        interval: 5 * 1000,
+        getFeatureId: function(f) {
+            return f.properties.id;
         },
-        geometry: {
-            type: 'LineString',
-            coordinates: []
-        }
-    },
-    
-    realtime = L.realtime(function(success, error) {
-        L.Realtime.reqwest({
-            url: 'http://localhost:8080/api/status',
-            type: 'json'
-        })
-        .then(function(data) {
-            var trailCoords = trail.geometry.coordinates;
-            trailCoords.push(data.geometry.coordinates);
-            trailCoords.splice(0, Math.max(0, trailCoords.length - 5));
-            success({
-                type: 'FeatureCollection',
-                features: [data, trail]
+        cache: true,
+        container: container,
+        onEachFeature(f, l) {
+            date = f.properties.date;
+            l.bindPopup(date);
+            l.on("mouseover", function () {
+                l.openPopup();
             });
-        })
-        .catch(error);
-    }, {
-        interval: 3 * 1000
-    }).addTo(map);
+            l.on("mouseout", function () {
+                l.closePopup();
+            });
+        }
+    });
+}
+    realtime1 = createRealtimeLayer('getPosition').addTo(map),
+    realtime2 = createRealtimeLayer('getUserPositionHistory').addTo(map);
 
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+L.control.layers(null, {
+    'Current': realtime1,
+    'History': realtime2
 }).addTo(map);
 
-realtime.on('update', function() {
-    map.fitBounds(realtime.getBounds(), {maxZoom: 3});
+realtime1.once('update', function() {
+    map.fitBounds(realtime1.getBounds(), {maxZoom: 18});
 });
